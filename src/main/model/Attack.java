577a51155c33e52daeb42db6a3b88d622de787f0
Wendi <liu.wendi@yahoo.ca>
2024-04-06 14:20:02 -0700

    Create Attack and Main

--- src/main/model/Attack.java
+++ src/main/model/Attack.java
@@ -1,1 +1,36 @@
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Attack class
+ */
+public class Attack {
+    private static final int BASE_ATTACK = 25;
+
+    private int attackDamage;
+    private boolean boosted;
+    private static List<String> attackMessages;
+
+    public Attack() {
+        attackDamage = BASE_ATTACK;
+        boosted = false;
+        attackMessages = new ArrayList<>();
+        attackMessages.add("You studied hard!");
+        attackMessages.add("You made progress!");
+        attackMessages.add("Keep up the good work!");
+    }
+
+    public int getAttackDamage() {
+        return attackDamage;
+    }
+
+    public boolean getBoosted() {
+        return boosted;
+    }
+
+    public String getRandomMessage() {
+        int randomNum = ThreadLocalRandom.current().nextInt(0, attackMessages.size());
+        return attackMessages.get(randomNum);
+    }
+    
+}
