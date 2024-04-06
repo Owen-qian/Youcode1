6fa5a2c2731bf8bff07847a2e71f7cdf0e25f35e
Zeqian <zfeng11@students.cs.ubc.ca>
2024-04-06 14:15:10 -0700

    add defence

--- src/main/model/Defence.java
+++ src/main/model/Defence.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Defence {
-    
+    private int amount;
+    private double multiplier;
+    private List<String> actions = new ArrayList<>();
+    public Defence (int amount,double multiplier){
+        this.amount = amount;
+        this.multiplier = multiplier;
+        actions.add("go hiking");
+        actions.add("take a walk");
+        actions.add("do exercises");
+        actions.add("go swimming");
+        actions.add("fave fun with friends");
+    }
+    public int getAmount(boolean buffed){
+        if(buffed){
+            return (int) (amount * multiplier);
+        }
+        return amount;
+    }
+    public void print(){
+        Random random = new Random();
+        System.out.println("You choose to "+actions.get(random.nextInt(actions.size()+1))+"! :)");
+    }
+
 }
