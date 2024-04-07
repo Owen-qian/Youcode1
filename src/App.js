322e3c502fe7b1c34744af8f2964bafaa7fe7c81
Zeqian <zfeng11@students.cs.ubc.ca>
2024-04-07 11:04:14 -0700

    11

--- src/App.js
+++ src/App.js
@@ -4,18 +4,9 @@ import Game from './Game.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to My Game</h1>
-      </header>
-      <main>
+    
         <Game />
-      </main>
-      <footer>
-        <p>© 2024 Your Game Name</p>
-      </footer>
-    </div>
+    
   );
 }
 
